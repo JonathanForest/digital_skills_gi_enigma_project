e8c4ea02d2fc66261970a424d989b3ac0b121c5c
Jonathan <jonathan@forestandrock.co.uk>
2023-04-16 20:18:51 +0200

    small changes

--- machine.py
+++ machine.py
@@ -1,6 +1,12 @@
-from .settings import *
+from settings import *
 from enigma.machine import EnigmaMachine
 
+
+# The following few lines create an 'instance' of an Enigma Machine. We
+# imported the blueprints earlier, now we are telling Python to use those
+# blueprints to make a version of the Enigma Machine we can interact with.
+# 'Enigma Machine' is the class 'template', 'from_key_sheet' is a function
+# belonging to the EnigmaMachine class. What arguments are we passing to it?
 machine = EnigmaMachine.from_key_sheet(
     rotors=ROTORS,
     reflector=REFLECTOR,
--- main.py
+++ main.py
@@ -2,21 +2,15 @@
 # Here, we import the EnigmaMachine class into our file so that we can use it
 # later. We're importing it from the enigma module that we installed earlier
 # using 'pip install ..
-from .settings import ROTORS, REFLECTOR, RING_SETTINGS, PLUGBOARD
-from .machine import machine
-from .decrypt import brute_force_rotor_position
-
-# The following few lines create an 'instance' of an Enigma Machine. We
-# imported the blueprints earlier, now we are telling Python to use those
-# blueprints to make a version of the Enigma Machine we can interact with.
-# 'Enigma Machine' is the class 'template', 'from_key_sheet' is a function
-# belonging to the EnigmaMachine class. What arguments are we passing to it?
+from settings import ROTORS, REFLECTOR, RING_SETTINGS, PLUGBOARD
+from machine import machine
+from decrypt import brute_force_rotor_position
 
 # Here we are loading the message we need to decrypt.
 with open("message.txt", mode="r") as f:
     message = f.read()
 
-result = brute_force_rotor_position(ROTORS, message, "WETTER")
+result = brute_force_rotor_position(machine, message, "WETTER")
 
 if result != 0:
     machine.set_display(result)
